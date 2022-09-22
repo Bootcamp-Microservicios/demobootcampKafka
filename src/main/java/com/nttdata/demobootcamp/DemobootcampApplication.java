baa40753ddda22a65a30cf4239905ec234215e2a
fichpas <fichpasb@emeal.nttdata.com>
2022-09-21 20:17:45 -0500

    Feat: Add Kafka producer and consumer

---
+++ src/main/java/com/nttdata/demobootcamp/DemobootcampApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.demobootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobootcampApplication.class, args);
+	}
+
+}
